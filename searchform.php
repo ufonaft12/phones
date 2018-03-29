845d19b00a88f3dc2fe41b28c8281e3405c33a35
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-29 07:22:22 +0300

    start project PHONES

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form form" action="">
+	<label for="form-search-input" class="sr-only"></label>
+	<div class="input-group">
+		<input type="text" id="form-search-input" class="form-control" placeholder="" value="" name="s" title="">
+		<span class="input-group-btn">
+			<button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
+		</span>
+	</div>
+</form>
