4ab6d0e66e4bb0755a75e871175cc08a3d1b502f
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-30 11:51:02 +0300

    layout header

--- header.php
+++ header.php
@@ -12,13 +12,13 @@ function get_header($page_type) { ?>
 	<link rel='stylesheet' id='wp-css'  href='/wp/css/wp.css?ver=4.9.4' type='text/css' media='all' />
 	
 	<!--STANDART-->
-	<link rel='stylesheet' id='bootstrap4-css'  href='/css/bootstrap.min.css?ver=4.0.0' type='text/css' media='all' />
+<!--	<link rel='stylesheet' id='bootstrap4-css'  href='inc/css/bootstrap.min.css?ver=4.0.0' type='text/css' media='all' />-->
 	<script type='text/javascript' src='/inc/js/jquery.js?ver=1.12.4'></script>
 	<script type='text/javascript' src='/inc/js/jquery-migrate.min.js?ver=1.4.1'></script>
 	<script type='text/javascript' src='/inc/js/bootstrap.min.js?ver=4.0.0'></script>
 	
 	<!--FONTS-->
-	<link rel='stylesheet' id='fontawesome-css'  href='/css/fontawesome-all.min.css?ver=5.0.7' type='text/css' media='all' />
+	<link rel='stylesheet' id='fontawesome-css'  href='inc/css/fontawesome-all.min.css?ver=5.0.7' type='text/css' media='all' />
 	<link rel='stylesheet' id='linearicons-css'  href='/css/linearicons-free.min.css?ver=1.0.0' type='text/css' media='all' />
 	<link rel='stylesheet' id='almoni-dl-aaa-css'  href='/inc/css/almoni-dl-aaa.min.css?ver=1.0.0' type='text/css' media='all' />
 	<link rel='stylesheet' id='montserrat-css'  href='/inc/css/montserrat.min.css?ver=1.0.0' type='text/css' media='all' />
@@ -34,55 +34,89 @@ function get_header($page_type) { ?>
 </head>
 <body class="rtl<?php echo ' '.$page_type; ?>">
 	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 content">
-					<a class="mobile-menu-btn" href="#menu">
-						<span></span>
-						<span></span>
-						<span></span>
-					</a>
-					<div class="col-logo col-right col-table">
-						<a class="vertical-center" href="/" alt="">
-							<img src="/img/logo.png" alt="" title="">
-						</a>
-					</div>
-					<div id="menu" class="col-menu col-right">
-						<ul id="menu-header" class="menu-header">
-							<li class="menu-item menu-item-has-children">
-								<a href="#">השכרת אוהלים</a>
-								<ul class="sub-menu">
-									<li class="menu-item">
-										<a href="#">צור קשר</a>
-									</li>
-									<li class="menu-item">
-										<a href="#">צור קשר</a>
-									</li>
-									<li class="menu-item">
-										<a href="#">צור קשר</a>
-									</li>
-									<li class="menu-item">
-										<a href="#">צור קשר</a>
-									</li>
-								</ul>
-							</li>
-							<li class="menu-item">
-								<a href="#">השכרת ציוד לאירועים</a>
-							</li>
-							<li class="menu-item">
-								<a href="#">אזורי שירות</a>
-							</li>
-							<li class="menu-item ">
-								<a href="#">צור קשר</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-info col-left">
-
-					</div>
-				</div>
-			</div>
-		</div>
+        <div class="top-line">
+            <div class="search-form">
+                <form role="search" method="get" class="search-form form" action="">
+                    <label for="form-search-input" class="sr-only"></label>
+                    <div class="input-group">
+                        <input type="text" id="form-search-input" class="form-control" placeholder="חיפוש מהיר" value="" name="s" title="">
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div class="social-block">
+                <ul>
+                    <li><a href="" target="_blank"><i class="fab fa-google-plus-g"></i></a></li>
+                    <li><a href="" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-line">
+            <div class="container">
+                <!--			<div class="row">-->
+                <!--				<div class="col-md-12 content">-->
+                <a class="mobile-menu-btn" href="#menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+                <div class="col-logo col-right col-table">
+                    <a class="vertical-center" href="/" alt="">
+                        <img src="img/logo.png" alt="logo" title="logo">
+                    </a>
+                </div>
+                <div id="menu" class="col-menu col-right">
+                    <ul id="menu-header" class="menu-header">
+                        <li class="menu-item home-icon">
+                            <a href="#">אודות</a>
+                        </li>
+                        <li class="menu-item about-icon">
+                            <a href="#">שירותי תיקונים</a>
+                        </li>
+                        <li class="menu-item menu-item-has-children repair-icon">
+                            <a href="#">השכרת אוהלים</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item">
+                                    <a href="#">צור קשר</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">צור קשר</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">צור קשר</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#">צור קשר</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item bye-icon">
+                            <a href="#">שירותי מכירה</a>
+                        </li>
+                        <li class="menu-item contact-icon">
+                            <a href="#">צור קשר</a>
+                        </li>
+                        <li class="menu-item service-icon">
+                            <a href="#">קריאת שירות</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-info col-left">
+                    <div class="tel-left">
+                        <span><a href="">052-8844223</a></span>
+                    </div>
+                    <div class="tel-right">
+                        <span><a href="">052-8844223</a></span>
+                    </div>
+                </div>
+                <!--				</div>-->
+                <!--			</div>-->
+            </div>
+        </div>
 	</header>
 	<div class="page-content">
+    <link rel='stylesheet' id='main-style-css'  href='/css/header.css' type='text/css' media='all' />
 <?php } ?>
